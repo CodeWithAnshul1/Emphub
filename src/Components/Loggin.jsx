bf42b86b2b89c27719c4382d57a17447e56798be
Anshul Rajput <anshulmogha50@gmail.com>
2026-03-22 20:03:10 +0530

    fix: syntex

--- src/Components/Loggin.jsx
+++ src/Components/Loggin.jsx
@@ -21,6 +21,7 @@ export default function Loggin() {
           headers: {
             "Content-Type": "application/json",
           },
+
           body: JSON.stringify({ email, password }),
         });
         
