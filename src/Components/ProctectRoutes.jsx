91969d8f4e5a0e1cdecaf26c2ae69e783fb32480
Anshul Rajput <anshulmogha50@gmail.com>
2026-03-31 12:25:05 +0530

    fix error

--- src/Components/ProctectRoutes.jsx
+++ src/Components/ProctectRoutes.jsx
@@ -1,16 +1,23 @@
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/Authcontext";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectRoutes({ children }) {
-  const { user, loading } = useAuth(); // ✅ inside function
+  const { user, loading } = useAuth();
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
+  // ❌ not logged in
   if (!user) {
     return <Navigate to="/" />;
   }
 
+  // ❌ logged in but NOT admin
+  if (user.role !== "admin" && user.role !== "superadmin") {
+    return <Navigate to="/clint" />; // redirect normal user
+  }
+
+  // ✅ admin / superadmin allowed
   return children;
 }
